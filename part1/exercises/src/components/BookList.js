6d0a3957e0549b1ba691a93bcec568ea4f3ea933
Angela Engle <angelaengle67@gmail.com>
2023-07-05 14:25:27 -0500

    Part 1 Complete

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://m.media-amazon.com/images/I/91ocU8970hL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71DWj7cP0ML._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/81lAPl9Fl0L._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Harry Potter and the Sorcerer's Stone" />
+         <img src={book2} alt="Harry Potter and the Chamber of Secrets" />
+         <img src={book3} alt="Harry Potter and the Prisoner of Azkaban" />
       </div>      
    );
 }
