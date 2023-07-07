70cffd9b8e87cc6349c517d95ebcba85b455abe7
Angela Engle <angelaengle67@gmail.com>
2023-07-07 10:09:30 -0500

    Complete

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,5 @@
+export default function HobbyLinks(){
+    let hobbyLinks = ["https://store.steampowered.com/","https://www.netflix.com/"]
+    return (<div><a href={hobbyLinks[0]}>Videogames</a>,
+    <a href={hobbyLinks[1]}>Netflix</a></div>)
+};
